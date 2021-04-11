c604b14b7f307742276236d3b8f9e990c779baa8
yasser-hub <78084724+yasser-hub@users.noreply.github.com>
2021-04-12 02:09:10 +0300

    test

--- Election/src/main/java/signup.java
+++ Election/src/main/java/signup.java
@@ -1,4 +1,4 @@
-
+ 
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Dao.signupDao;
+import data.users;
 
 /**
  * Servlet implementation class signup
@@ -34,8 +37,17 @@ public class signup extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		String pass1 = request.getParameter("pass1");
+	 	String pass2 = request.getParameter("pass2");
+	 	String username = request.getParameter("username"); 
+	 	
+		 	users user= new users(username, pass1);
+		 	signupDao sDao = new signupDao();
+		 	String result = sDao.insert(user);
+	 		response.getWriter().println(result);
+
+	 	
 	}
 
 }
